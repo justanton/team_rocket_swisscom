a21e559e811794f7d7c965bd8ce3324c9e56e9ad
Klaus Deja <klaus.deja@gmail.com>
2017-03-18 15:19:30 +0100

    capturing voice client side

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,30 @@
-(function() {
-    //insert content
-})();
+($('document').ready(function () {
+    const microm = new Microm();
+    let mp3 = null;
+    let record = $('#record');
+    let play = $('#play');
+    let stoprecord = $('#stoprecord');
+
+    record.click(() => {
+        microm.record().then(function () {
+            record.hide();
+            stoprecord.show();
+            console.log('recording...')
+        }).catch(function () {
+            console.log('error recording');
+        });
+    })
+
+    stoprecord.click(() => {
+        microm.stop().then(function (result) {
+            stoprecord.hide();
+            record.show();
+            mp3 = result;
+            console.log(mp3.url, mp3.blob, mp3.buffer);
+        })
+    })
+
+    play.click(() => {
+        microm.play();
+    })
+}));
