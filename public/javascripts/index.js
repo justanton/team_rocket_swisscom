77b4dc8c205434b65ec419872f78931dc7e86d46
Klaus Deja <klaus.deja@gmail.com>
2017-03-18 22:57:31 +0100

    Basic game working

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,15 +1,51 @@
 ($('document').ready(function () {
+    // request mic permission
+    navigator.mediaDevices.getUserMedia({audio: true});
     const microm = new Microm();
     let mp3 = null;
-    let record = $('#record');
-    let play = $('#play');
-    let stoprecord = $('#stoprecord');
+    let askButton = $('#ask');
+    let voicePermission = $('#voicepermission');
+    let gameContainer = $('#gamecontainer');
+    let questionContainer = $('#questioncontainer');
+    let startGameButton = $('#startgame');
+    let nextButton = $('#nextbutton');
+    let micIcon = $('#micicon');
 
-    let onSuccess = function() {
-        alert('upload successful');
-    }
+    const getQuestion = () => {
+        const questions = [
+            'sprayed a grafitti',
+            'cheated in school',
+            'forgot homework',
+            'eaten mushrooms',
+            'celebrated my birthday',
+            'been at a hackathon'
+        ];
 
-    let uploadVoice = function () {
+        return questions[Math.floor(Math.random() * questions.length)];
+    };
+
+    const showNextButton = () => {
+        nextButton.show();
+    };
+
+    const startNewGame = () => {
+            voicePermission.show();
+            nextButton.hide();
+            startGameButton.hide();
+            navigator.mediaDevices.getUserMedia({audio: true}).then((stream) => {
+                doTransition(() => {
+                    askButton.show();
+                    voicePermission.hide();
+                    questionContainer.text(getQuestion());
+                    gameContainer.show();
+                });
+            }).catch(function(err) {
+                /* handle the error */
+            });
+
+    };
+
+    const uploadVoice = () => {
         microm.getMp3().then(function (data) {
             var fd = new FormData();
             fd.append('data', data.blob);
@@ -19,32 +55,44 @@
                 contentType: false,
                 url: '/upload_voice',
                 data: fd,
-                success: onSuccess
+                success: showNextButton
             });
         });
     };
 
-    record.click(() => {
+    const doTransition = (callback) => {
+        $('body').toggleClass('magictime rotateUp');
+        setTimeout(() => {
+            $('body').toggleClass('magictime rotateUp');
+            if (callback)
+                callback();
+        }, 400)
+    };
+
+    const stopRecording = () => {
+        microm.stop().then(function (result) {
+            mp3 = result;
+            console.log(mp3.url, mp3.blob, mp3.buffer);
+            showNextButton();
+            micIcon.hide();
+            uploadVoice(result);
+        });
+    };
+
+    askButton.click(() => {
+        askButton.hide();
+        micIcon.show();
         microm.record().then(function () {
-            record.hide();
-            stoprecord.show();
-            console.log('recording...')
-        }).catch(function () {
-            console.log('error recording');
+            setTimeout(stopRecording, 5000);
         });
     });
 
-    stoprecord.click(() => {
-        microm.stop().then(function (result) {
-            stoprecord.hide();
-            record.show();
-            mp3 = result;
-            console.log(mp3.url, mp3.blob, mp3.buffer);
-            uploadVoice(result);
-        })
-    })
+    startGameButton.click( () => {
+        startNewGame();
+    });
 
-    play.click(() => {
-        microm.play();
-    })
+    nextButton.click( () => {
+        startNewGame();
+    });
 }));
+
